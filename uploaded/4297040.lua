249d919d1f585b3ebd296278485510e5b22fea2e
thecloudyy <itsaseal1@gmail.com>
2026-08-17 15:25:11 +0300

    Update: Add/modify Lua files

---
+++ uploaded/4297040.lua
@@ -0,0 +1,22 @@
+-- 4297040's Lua and Manifest Created by Hubcap Manifest
+-- Aeternum Infecti
+-- Created: July 30, 2026 at 23:18:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4297040) -- Aeternum Infecti
+-- MAIN APP DEPOTS
+addappid(4297041, 1, "5742b020d51f678796e7d66e0b6d06181c9289b7e21ea5e516906f1c132ac002") -- Depot 4297041
+setManifestid(4297041, "8738996128042071721", 24625762028)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4297042) -- Depot 4297042 (empty depot)
