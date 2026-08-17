53ae08adeee19251bf7fe51840e628a3c15f5e0e
thecloudyy <itsaseal1@gmail.com>
2026-08-17 15:22:16 +0300

    Update: Add/modify Lua files

---
+++ uploaded/4189870.lua
@@ -0,0 +1,12 @@
+-- 4189870's Lua and Manifest Created by Hubcap Manifest
+-- RepeatsPC-Motor boat
+-- Created: February 22, 2026 at 00:48:31 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4189870) -- RepeatsPC-Motor boat
+-- MAIN APP DEPOTS
+addappid(4189871, 1, "f0dcacbf5ab221227580393852c2c205bbe275b34daf0b6a254adc4dc59f5e62") -- Depot 4189871
+setManifestid(4189871, "7786644755443791501", 3091741006)
