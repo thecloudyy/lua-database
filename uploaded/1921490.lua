8747a471d549cccd313b99e4ccf379960bcdaf1f
thecloudyy <itsaseal1@gmail.com>
2026-08-18 14:20:04 +0300

    Update Lua files

--- uploaded/1921490.lua
+++ uploaded/1921490.lua
@@ -1,12 +1,3 @@
--- 1921490's Lua and Manifest Created by Hubcap Manifest
--- Wild Blue Skies
--- Created: August 14, 2026 at 10:44:12 EDT
--- Website: https://hubcapmanifest.com/
--- Total Depots: 1
--- Total DLCs: 0
-
--- MAIN APPLICATION
-addappid(1921490) -- Wild Blue Skies
--- MAIN APP DEPOTS
-addappid(1921491, 1, "fa9731c470e6384d128d5847b038b59fb76b9ea5288ae9584b9861a1ebfd88c4") -- Depot 1921491
-setManifestid(1921491, "1526186745412442091", 1795803449)
+addappid(1921490)
+addappid(1921490,0,"fa9731c470e6384d128d5847b038b59fb76b9ea5288ae9584b9861a1ebfd88c4")
+-- setManifestid(1921491, "1526186745412442091", 1795803449)
--- uploaded/292000.lua
+++ uploaded/292000.lua
@@ -1,18 +1,7 @@
--- 292000's Lua and Manifest Created by Hubcap Manifest
--- No More Room in Hell 2
--- Created: August 11, 2026 at 09:33:18 EDT
--- Website: https://hubcapmanifest.com/
--- Total Depots: 3
--- Total DLCs: 0
--- Shared Depots: 2
-
--- MAIN APPLICATION
-addappid(292000, 1, "4a7cdda160ead514fee5cc6330491cea03bcfd7a56ff3696ccbf51568cf18069") -- No More Room in Hell 2
--- MAIN APP DEPOTS
-addappid(292001, 1, "2bb3dfcc7245e9a85e6adcb82c91257dfd6e3ca66bdc188476820020395cc812") -- Depot 292001
-setManifestid(292001, "8335295176305038937", 25159978658)
--- SHARED DEPOTS (from other apps)
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
-setManifestid(228989, "5753583882400741046", 25674515)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
-setManifestid(228990, "1829726630299308803", 102931551)
+addappid(292000, 1, "4a7cdda160ead514fee5cc6330491cea03bcfd7a56ff3696ccbf51568cf18069")
+addappid(292001, 1, "2bb3dfcc7245e9a85e6adcb82c91257dfd6e3ca66bdc188476820020395cc812")
+-- setManifestid(292001, "8335295176305038937", 25159978658)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+-- setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+-- setManifestid(228990, "1829726630299308803", 102931551)
